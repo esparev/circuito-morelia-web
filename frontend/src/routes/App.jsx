c250032f294856722bdc7c7c8f406bb5eaf5515d
JoseMa <esparev@hotmail.com>
2022-11-12 15:14:56 -0600

    refactor: ♻️ agregar todas las rutas del sitio

--- frontend/src/routes/App.jsx
+++ frontend/src/routes/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '@containers/Layout';
+import Login from '@containers/Login';
+import Signup from '@containers/Signup';
 import Home from '@containers/Home';
+import Admins from '@containers/Admins';
+import Admin from '@containers/Admin';
+import Drivers from '@containers/Drivers';
+import Driver from '@containers/Driver';
+import Units from '@containers/Units';
+import Unit from '@containers/Unit';
+import Stops from '@containers/Stops';
 
 const App = () => {
   return (
@@ -9,6 +18,15 @@ const App = () => {
       <Layout>
         <Routes>
           <Route index element={<Home />} />
+          <Route path='admins' element={<Admins />} />
+          <Route path='admins/:slug' element={<Admin />} />
+          <Route path='conductores' element={<Drivers />} />
+          <Route path='conductores/:slug' element={<Driver />} />
+          <Route path='unidades' element={<Units />} />
+          <Route path='unidades/:slug' element={<Unit />} />
+          <Route path='paradas' element={<Stops />} />
+          <Route path='inicia-sesion' element={<Login />} />
+          <Route path='crear-cuenta' element={<Signup />} />
         </Routes>
       </Layout>
     </BrowserRouter>
