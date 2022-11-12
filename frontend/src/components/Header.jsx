83922de04d4844ea4e70a69e8b0ba8c8db80f573
JoseMa <esparev@hotmail.com>
2022-11-12 15:36:06 -0600

    feat: ✨ implementar funcionalidad de routas en el header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import LogoutButton from '@components/LogoutButton';
 import LogoCircuito from '@img/Circuito_Morelia_Logo.png';
 import '@styles/Header.css';
@@ -22,13 +23,9 @@ const Header = () => {
     <header className='header'>
       <div className='header__buttons'>
         <figure className='header__figure'>
-          <a href='./index.html'>
-            <img
-              className='header__figure--img'
-              src={LogoCircuito}
-              alt='Logo Circuito Morelia'
-            />
-          </a>
+          <Link to='/'>
+            <img className='header__figure--img' src={LogoCircuito} alt='Logo Circuito Morelia' />
+          </Link>
         </figure>
 
         <div className='header__mobile-btn'>
@@ -36,8 +33,7 @@ const Header = () => {
             className='header__hamburger'
             type='button'
             aria-label='Toggle menu'
-            id='nav-toggle'
-          >
+            id='nav-toggle'>
             <svg
               className={
                 isActive
@@ -45,8 +41,7 @@ const Header = () => {
                   : 'header__hamburger--btn ham hamRotate ham1'
               }
               viewBox='0 0 100 100'
-              onClick={handleToggle}
-            >
+              onClick={handleToggle}>
               <path
                 className='line top'
                 d='m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40'
@@ -63,27 +58,26 @@ const Header = () => {
 
       <ul className='menu' id='menu'>
         <li className='menu__item'>
-          <a href=''>Paradas</a>
+          <Link to='/paradas'>Paradas</Link>
         </li>
         <li className='menu__item'>
-          <a href=''>Unidades</a>
+          <Link to='/unidades'>Unidades</Link>
         </li>
         <li className='menu__item'>
-          <a href=''>Conductores</a>
+          <Link to='/conductores'>Conductores</Link>
         </li>
         <li className='menu__item'>
-          <a href=''>Perfil</a>
+          <Link to='/perfil'>Perfil</Link>
         </li>
         <li className='menu__item'>
-          <a className='menu__logout--svg' href='' title='Cerrar sesión'>
+          <Link className='menu__logout--svg' to='/incia-sesion' title='Cerrar sesión'>
             <svg
               className='icon--24 icon--gray'
               width='72'
               height='72'
               viewBox='0 0 72 72'
               fill='none'
-              xmlns='http://www.w3.org/2000/svg'
-            >
+              xmlns='http://www.w3.org/2000/svg'>
               <path
                 d='M45 9H17C15.8954 9 15 9.89543 15 11V61C15 62.1046 15.8954 63 17 63H45'
                 strokeWidth='6'
@@ -97,7 +91,7 @@ const Header = () => {
                 strokeLinejoin='round'
               />
             </svg>
-          </a>
+          </Link>
           <LogoutButton />
         </li>
       </ul>
