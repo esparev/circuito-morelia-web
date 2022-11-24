97c13f7db5d5e734aa9275671a7424c39dd9ebd5
JoseMa <esparev@hotmail.com>
2022-11-24 17:16:01 -0600

    feat: ✨ agregar seeds para poblar la db

---
+++ backend/db/seeders/20221124212410-super-admin.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { USER_TABLE } = require('../models/user.model');
+
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert(USER_TABLE, [
+			{
+				slug: 'esparev',
+				name: 'JoseMa Esparev',
+				email: 'esparev@hotmail.com',
+				password:
+					'$2b$13$oP9JNy3eHStLEdrTc9KDR.sMz9R2cwntXEFR0Yc3AwT3yhMswtbuO',
+				role: 'hero',
+				image: '',
+			},
+			{
+				slug: 'lpaz',
+				name: 'Lupita Paz',
+				email: 'lpaz@hotmail.com',
+				password:
+					'$2b$13$oP9JNy3eHStLEdrTc9KDR.sMz9R2cwntXEFR0Yc3AwT3yhMswtbuO',
+				role: 'hero',
+				image: '',
+			},
+			{
+				slug: 'eljhonnyx',
+				name: 'Jhonatan Rodriguez',
+				email: 'brandon@hotmail.com',
+				password:
+					'$2b$13$oP9JNy3eHStLEdrTc9KDR.sMz9R2cwntXEFR0Yc3AwT3yhMswtbuO',
+				role: 'hero',
+				image: '',
+			},
+		]);
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete(USER_TABLE, null);
+	},
+};
