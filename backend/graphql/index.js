6df446b22ce51c5fc689bf56a3bf9221af4192d0
JoseMa <esparev@hotmail.com>
2023-03-30 16:59:29 -0600

    construction: 🚧 crear primera consulta con graphql

---
+++ backend/graphql/index.js
@@ -0,0 +1,30 @@
+const { ApolloServer } = require('apollo-server-express');
+const {
+	ApolloServerPluginLandingPageGraphQLPlayground,
+} = require('apollo-server-core');
+
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+	Query: {
+		hello: () => 'Hello world!',
+	},
+};
+
+const useGraphQL = async (app) => {
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		playground: true,
+		plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+	});
+
+	await server.start();
+	server.applyMiddleware({ app });
+};
+
+module.exports = useGraphQL;
